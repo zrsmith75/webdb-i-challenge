da4bd1c54a94ea8480de54f7bfe5b36fde2b3634
Zac Smith <zrs3141592@gmail.com>
2019-08-21 18:38:41 -0600

    GET working correctly

--- server.js
+++ server.js
@@ -1,32 +1,30 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
+const db = require("./data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
 
+server.get("/", (req, res) => {
+  // db.select('*').from('accounts');
+  db("accounts")
+    .then(accounts => {
+      res.status(200).json(accounts);
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Unable to GET your account"
+      });
+    });
+});
 
-server.get('/', (req, res) => {
+server.get("/:id", (req, res) => {});
 
-})
+server.post("/", (req, res) => {});
 
-server.get('/:id', (req, res) => {
+server.put("/:id", (req, res) => {});
 
-})
-
-server.post('/', (req, res) => {
-
-})
-
-server.put('/:id', (req, res) => {
-
-})
-
-server.delete('/:id'. (req, res) => {
-
-})
-
-
+server.delete("/:id", (req, res) => {});
 
 module.exports = server;
