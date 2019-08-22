2addf4150b9d20579bc1390d6ec9ada6c33f2e7d
Zac Smith <zrs3141592@gmail.com>
2019-08-21 18:31:28 -0600

    set up CRUD

--- server.js
+++ server.js
@@ -6,4 +6,27 @@ const server = express();
 
 server.use(express.json());
 
+
+server.get('/', (req, res) => {
+
+})
+
+server.get('/:id', (req, res) => {
+
+})
+
+server.post('/', (req, res) => {
+
+})
+
+server.put('/:id', (req, res) => {
+
+})
+
+server.delete('/:id'. (req, res) => {
+
+})
+
+
+
 module.exports = server;
